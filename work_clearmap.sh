14ee40416600bd1a115860190e086ac9d61c6614
ubukawa <ubukawa-t96dq@mlit.go.jp>
2021-06-28 22:57:42 -0400

    Update work_clearmap.sh

--- work_clearmap.sh
+++ work_clearmap.sh
@@ -1,3 +1,3 @@
-node produce-small/index.js
+node produce-clearmap/index-clear.js
 /usr/local/bin/tile-join --no-tile-size-limit -e produce-clearmap/clearmapVT produce-clearmap/clearmaptile/0-0-0.mbtiles --no-tile-compression
 
