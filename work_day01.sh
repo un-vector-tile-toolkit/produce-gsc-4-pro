dfd15464b8e9671b378a1fd9cbf45b392ca79550
ubukawa <ubukawa-t96dq@mlit.go.jp>
2021-08-23 12:20:15 -0400

    modified the path

--- work_day01.sh
+++ work_day01.sh
@@ -1,3 +1,3 @@
 node produce-gsc-osm-46/index_day01.js
-for f in produce-gsc-osm-46/mbtiles/osm_tile_day01/*.mbtiles; do tile-join --no-tile-size-limit -f -o large_tiles/unosm/tile_day01/`basename ${f}` produce-gsc-un-46/mbtiles/un_tile/`basename ${f}` produce-gsc-osm-46/mbtiles/osm_tile_day01/`basename ${f}`; date; echo  `basename ${f}`; ls -alh large_tiles/unosm/tile_day01/`basename ${f}` ;done
+for f in produce-gsc-osm-46/mbtiles/osm_tile_day01/*.mbtiles; do /usr/local/bin/tile-join --no-tile-size-limit -f -o large_tiles/unosm/tile_day01/`basename ${f}` produce-gsc-un-46/mbtiles/un_tile/`basename ${f}` produce-gsc-osm-46/mbtiles/osm_tile_day01/`basename ${f}`; date; echo  `basename ${f}`; ls -alh large_tiles/unosm/tile_day01/`basename ${f}` ;done
 scp -i XXX(path to your ssh key) -r ./large_tiles/unosm/tile_day01/* (username)@(hostingserver):(path)/mbtiles/unosm
--- work_day07.sh
+++ work_day07.sh
@@ -1,3 +1,3 @@
 node produce-gsc-osm-46/index_day07.js
-for f in produce-gsc-osm-46/mbtiles/osm_tile_day07/*.mbtiles; do tile-join --no-tile-size-limit -f -o large_tiles/unosm/tile_day07/`basename ${f}` produce-gsc-un-46/mbtiles/un_tile/`basename ${f}` produce-gsc-osm-46/mbtiles/osm_tile_day07/`basename ${f}`; date; echo  `basename ${f}`; ls -alh large_tiles/unosm/tile_day07/`basename ${f}` ;done
+for f in produce-gsc-osm-46/mbtiles/osm_tile_day07/*.mbtiles; do /usr/local/bin/tile-join --no-tile-size-limit -f -o large_tiles/unosm/tile_day07/`basename ${f}` produce-gsc-un-46/mbtiles/un_tile/`basename ${f}` produce-gsc-osm-46/mbtiles/osm_tile_day07/`basename ${f}`; date; echo  `basename ${f}`; ls -alh large_tiles/unosm/tile_day07/`basename ${f}` ;done
 scp -i XXX(path to your ssh key) -r ./large_tiles/unosm/tile_day07/* (username)@(hostingserver):(path)/mbtiles/unosm
