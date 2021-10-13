566609bda64f7c2cfc89393782ac1ae1209e5dcc
ubukawa <ubukawa-t96dq@mlit.go.jp>
2021-10-13 17:15:14 -0400

    ZL16

--- work_day03.sh
+++ work_day03.sh
@@ -1,3 +1,5 @@
-node produce-gsc-osm-46/index_day03.js
-for f in produce-gsc-osm-46/mbtiles/osm_tile_day03/*.mbtiles; do /usr/local/bin/tile-join --no-tile-size-limit -f -o large_tiles/unosm/tile_day03/`basename ${f}` produce-gsc-un-46/mbtiles/un_tile/`basename ${f}` produce-gsc-osm-46/mbtiles/osm_tile_day03/`basename ${f}`; date; echo  `basename ${f}`; ls -alh large_tiles/unosm/tile_day03/`basename ${f}` ;done
+# node produce-gsc-osm-46/index_day03.js
+# for f in produce-gsc-osm-46/mbtiles/osm_tile_day03/*.mbtiles; do /usr/local/bin/tile-join --no-tile-size-limit -f -o large_tiles/unosm/tile_day03/`basename ${f}` produce-gsc-un-46/mbtiles/un_tile/`basename ${f}` produce-gsc-osm-46/mbtiles/osm_tile_day03/`basename ${f}`; date; echo  `basename ${f}`; ls -alh large_tiles/unosm/tile_day03/`basename ${f}` ;done
+node produce-gsc-osm-46/index_day03-z16.js
+for f in produce-gsc-osm-46/mbtiles/osm_tile_day03/*.mbtiles; do /usr/local/bin/tile-join --no-tile-size-limit -f -o large_tiles/unosm/tile_day03/`basename ${f}` produce-gsc-un-46/mbtiles/un_tile/`basename ${f}` produce-gsc-osm-46/mbtiles/osm_tile_day03/`basename ${f}`; rm produce-gsc-osm-46/mbtiles/osm_tile_day03/`basename ${f}`; date; echo  `basename ${f}`; ls -alh large_tiles/unosm/tile_day03/`basename ${f}` ;done
 scp -i XXX(path to your ssh key) -r ./large_tiles/unosm/tile_day03/* (username)@(hostingserver):(path)/mbtiles/unosm
--- work_day04.sh
+++ work_day04.sh
@@ -1,3 +1,5 @@
-node produce-gsc-osm-46/index_day04.js
-for f in produce-gsc-osm-46/mbtiles/osm_tile_day04/*.mbtiles; do /usr/local/bin/tile-join --no-tile-size-limit -f -o large_tiles/unosm/tile_day04/`basename ${f}` produce-gsc-un-46/mbtiles/un_tile/`basename ${f}` produce-gsc-osm-46/mbtiles/osm_tile_day04/`basename ${f}`; date; echo  `basename ${f}`; ls -alh large_tiles/unosm/tile_day04/`basename ${f}` ;done
+# node produce-gsc-osm-46/index_day04.js
+# for f in produce-gsc-osm-46/mbtiles/osm_tile_day04/*.mbtiles; do /usr/local/bin/tile-join --no-tile-size-limit -f -o large_tiles/unosm/tile_day04/`basename ${f}` produce-gsc-un-46/mbtiles/un_tile/`basename ${f}` produce-gsc-osm-46/mbtiles/osm_tile_day04/`basename ${f}`; date; echo  `basename ${f}`; ls -alh large_tiles/unosm/tile_day04/`basename ${f}` ;done
+node produce-gsc-osm-46/index_day04-z16.js
+for f in produce-gsc-osm-46/mbtiles/osm_tile_day04/*.mbtiles; do /usr/local/bin/tile-join --no-tile-size-limit -f -o large_tiles/unosm/tile_day04/`basename ${f}` produce-gsc-un-46/mbtiles/un_tile/`basename ${f}` produce-gsc-osm-46/mbtiles/osm_tile_day04/`basename ${f}`; rm produce-gsc-osm-46/mbtiles/osm_tile_day04/`basename ${f}`; date; echo  `basename ${f}`; ls -alh large_tiles/unosm/tile_day04/`basename ${f}` ;done
 scp -i XXX(path to your ssh key) -r ./large_tiles/unosm/tile_day04/* (username)@(hostingserver):(path)/mbtiles/unosm
